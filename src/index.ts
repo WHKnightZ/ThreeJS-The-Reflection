92e61569b986e3463099173d936b23decec32577
dealer <nguyenkhanhsl1997@gmail.com>
2022-12-01 18:48:33 +0700

    add player

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 import Stats from "three/examples/jsm/libs/stats.module";
-import { baseMap, CELL_SIZE, MAP_MAX_X, MAP_MAX_Y, SCREEN_HEIGHT, SCREEN_WIDTH } from "./configs/constants";
+import { baseMap, CELL_SIZE, Images, imageSrcs, MAP_MAX_X, MAP_MAX_Y, SCREEN_HEIGHT, SCREEN_WIDTH } from "./configs/constants";
 import { initMap, Map } from "./objects/map";
+import { Player } from "./objects/player";
+import { flipHorizontal } from "./utils/common";
 
 const stats: Stats = new (Stats as any)();
 document.body.appendChild(stats.dom);
@@ -16,8 +18,9 @@ let total = 0;
 let material: THREE.ShaderMaterial;
 let mesh: THREE.Mesh;
 let map: Map;
-
-let material2: any;
+let texture: THREE.Texture;
+let waterTexture: THREE.Texture;
+let player: Player;
 
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
@@ -27,8 +30,6 @@ canvas.width = SCREEN_WIDTH;
 canvas.height = SCREEN_HEIGHT;
 waterCanvas.width = SCREEN_WIDTH;
 waterCanvas.height = SCREEN_HEIGHT / 2;
-
-let texture2: any;
 
 const render = (now: number = 0) => {
   if (!then) then = now;
@@ -42,6 +43,7 @@ const render = (now: number = 0) => {
 
   context.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
   map.render();
+  player.render();
   waterContext.drawImage(canvas, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
 
   stats.update();
@@ -51,6 +53,37 @@ const render = (now: number = 0) => {
 };
 
 const init = async () => {
+  const rightImages: Images = { ...imageSrcs };
+  const leftImages: Images = { ...imageSrcs };
+
+  const loadImage = (key: string, src: string) => {
+    const image = new Image();
+    (rightImages as any)[key] = image;
+    image.src = `/static/images/${src}.png`;
+    return new Promise((res) => (image.onload = () => res(image)));
+  };
+
+  await Promise.all(Object.keys(rightImages).map((key) => loadImage(key, (rightImages as any)[key])));
+  const keys = Object.keys(rightImages);
+  (await Promise.all(keys.map((key) => flipHorizontal((rightImages as any)[key])))).forEach((image, index) => {
+    (leftImages as any)[keys[index]] = image;
+  });
+
+  const textures = [
+    [
+      [leftImages.stand0, leftImages.stand1, leftImages.stand2, leftImages.stand1],
+      [leftImages.run0, leftImages.run1, leftImages.run2, leftImages.run1],
+      [leftImages.jump0, leftImages.jump1],
+    ],
+    [
+      [rightImages.stand0, rightImages.stand1, rightImages.stand2, rightImages.stand1],
+      [rightImages.run0, rightImages.run1, rightImages.run2, rightImages.run1],
+      [rightImages.jump0, rightImages.jump1],
+    ],
+  ];
+
+  player = new Player(context, textures);
+
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas: rendererCanvas });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -65,19 +98,23 @@ const init = async () => {
 
   map = new Map(context, baseMap);
 
-  texture2 = new THREE.CanvasTexture(canvas);
+  texture = new THREE.CanvasTexture(canvas);
 
-  material2 = new THREE.MeshBasicMaterial({
-    map: texture2,
-  });
-  mesh = new THREE.Mesh(new THREE.PlaneGeometry(8, 4.8, 1, 1), material2);
+  mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 4.8, 1, 1),
+    new THREE.MeshBasicMaterial({
+      map: texture,
+    })
+  );
   mesh.position.set(0, 1.2, 0);
   scene.add(mesh);
+
+  waterTexture = new THREE.CanvasTexture(waterCanvas);
 
   material = new THREE.ShaderMaterial({
     uniforms: {
       uTime: { value: 0 },
-      uTexture: { value: new THREE.CanvasTexture(waterCanvas) },
+      uTexture: { value: waterTexture },
     },
     vertexShader: require("./shaders/vertex.glsl"),
     fragmentShader: require("./shaders/fragment.glsl"),
@@ -101,8 +138,6 @@ const drawMap = (e: MouseEvent) => {
   let x = Math.floor(((e.offsetX / CELL_SIZE) * rendererCanvas.width) / rendererCanvas.clientWidth);
   let y = Math.floor(((e.offsetY / CELL_SIZE) * rendererCanvas.height) / rendererCanvas.clientHeight);
 
-  // console.log(x, y);
-
   if (x < 0) x = 0;
   if (y < 0) y = 0;
   if (x >= MAP_MAX_X) x = MAP_MAX_X - 1;
@@ -113,9 +148,8 @@ const drawMap = (e: MouseEvent) => {
   baseMap[y][x] = newValue;
   map.init(baseMap);
 
-  texture2.needsUpdate = true;
-  // material2.map = new THREE.CanvasTexture(canvas);
-  // material.uniforms.uTexture.value = new THREE.CanvasTexture(waterCanvas);
+  texture.needsUpdate = true;
+  waterTexture.needsUpdate = true;
 };
 
 rendererCanvas.addEventListener("mousedown", (e) => {
